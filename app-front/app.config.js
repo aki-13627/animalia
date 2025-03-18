f8dbb1aa4175bb9e3732995beba47de7b47c38e5
aki-13627 <aki.kaku0627@gmail.com>
2025-03-18 19:51:44 +0900

    configのwarning解消

--- app-front/app.config.js
+++ app-front/app.config.js
@@ -4,6 +4,8 @@ export default {
   expo: {
     name: "app-front",
     slug: "app-front",
+    scheme: "animalia",
+    newArchEnabled: true,
     extra: {
       API_URL: process.env.API_URL,
     },
