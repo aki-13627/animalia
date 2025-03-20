df37f871be98b29d694eef6d56193004e0f612ac
aki-13627 <aki.kaku0627@gmail.com>
2025-03-20 17:27:41 +0900

    chore: 使ってないresponseTypeをなくす

--- backend-go/internal/domain/models/responses/auth_response.go
+++ backend-go/internal/domain/models/responses/auth_response.go
@@ -1,14 +1,5 @@
 package responses
 
-import "github.com/htanos/animalia/backend-go/internal/domain/models"
-
-type SignInResponse struct {
-	User         models.User
-	AccessToken  string
-	IdToken      string
-	RefreshToken string
-}
-
 type RefreshTokenResponse struct {
 	AccessToken string
 	IdToken     string
